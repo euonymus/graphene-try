d4937f0901b58567876d72acbdb0b9d3457d1175
euonymus <euonymus0220@gmail.com>
2018-12-02 12:35:24 +0900

    pagination

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -18,24 +18,38 @@ class VoteType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType, search=graphene.String())
+    links = graphene.List(
+        LinkType,
+        search=graphene.String(),
+        first=graphene.Int(),
+        skip=graphene.Int(),
+    )
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info, search=None, **kwargs):
+    # Use them to slice the Django queryset
+    def resolve_links(self, info, search=None, first=None, skip=None, **kwargs):
         # The value sent with the search parameter will be in the args variable
+        qs = Link.objects.all()
+
         if search:
             filter = (
                 Q(url__icontains=search) |
                 Q(description__icontains=search)
             )
-            return Link.objects.filter(filter)
+            qs = qs.filter(filter)
 
-        return Link.objects.all()
+        if skip:
+            qs = qs[skip:]
+
+        if first:
+            qs = qs[:first]
+
+        return qs
 
     def resolve_votes(self, info, **kwargs):
         return Vote.objects.all()
 
-
+    
 class CreateLink(graphene.Mutation):
     id = graphene.Int()
     url = graphene.String()
