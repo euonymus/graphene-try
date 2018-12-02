c1146abbb12e9eeb6b8291222e9d2924d622f0fa
euonymus <euonymus0220@gmail.com>
2018-12-02 12:22:31 +0900

    search

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -5,6 +5,7 @@ from .models import Link
 
 from users.schema import UserType
 from links.models import Link, Vote
+from django.db.models import Q
 
 
 class LinkType(DjangoObjectType):
@@ -17,14 +18,23 @@ class VoteType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
+    links = graphene.List(LinkType, search=graphene.String())
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info, **kwargs):
+    def resolve_links(self, info, search=None, **kwargs):
+        # The value sent with the search parameter will be in the args variable
+        if search:
+            filter = (
+                Q(url__icontains=search) |
+                Q(description__icontains=search)
+            )
+            return Link.objects.filter(filter)
+
         return Link.objects.all()
 
     def resolve_votes(self, info, **kwargs):
         return Vote.objects.all()
+
 
 class CreateLink(graphene.Mutation):
     id = graphene.Int()
