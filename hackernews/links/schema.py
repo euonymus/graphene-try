72b744d88875dd03ae75e1acccfbdb752e846972
euonymus <euonymus0220@gmail.com>
2018-12-03 11:17:45 +0900

    chore

--- hackernews/links/schema.py
+++ hackernews/links/schema.py
@@ -6,6 +6,7 @@ from .models import Link
 from users.schema import UserType
 from links.models import Link, Vote
 from graphql import GraphQLError
+from django.db.models import Q
 
 class LinkType(DjangoObjectType):
     class Meta:
@@ -17,14 +18,24 @@ class VoteType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    links = graphene.List(LinkType)
+    links = graphene.List(LinkType, search=graphene.String())
     votes = graphene.List(VoteType)
 
-    def resolve_links(self, info, **kwargs):
+    def resolve_links(self, info, search=None, **kwargs):
+        return Link.objects.all()
+
+        if search:
+            filter = (
+                Q(url__icontains=search) |
+                Q(description__icontains=search)
+            )
+            return Link.objects.filter(filter)
+
         return Link.objects.all()
 
     def resolve_votes(self, info, **kwargs):
         return Vote.objects.all()
+
 
 class CreateLink(graphene.Mutation):
     id = graphene.Int()
