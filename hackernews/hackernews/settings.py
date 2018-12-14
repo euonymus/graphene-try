133fcbebf24d751b3120fa157befec4da422b550
euonymus <euonymus0220@gmail.com>
2018-12-14 13:51:55 +0900

    postgresql

--- hackernews/hackernews/settings.py
+++ hackernews/hackernews/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'hackernews.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'graphql_test',
+        'USER': 'gluons',
+        'PASSWORD': 'gluons',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
